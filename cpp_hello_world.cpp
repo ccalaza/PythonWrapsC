12566bcef1f25d7ea8f135aa3a05ef39e0b92d55
ccalaza <cdgcalaza@gmail.com>
2017-03-20 14:50:15 +0100

    Update cpp_hello_world.cpp

--- cpp_hello_world.cpp
+++ cpp_hello_world.cpp
@@ -1,10 +1,9 @@
-
 /* The C functions to be called from run_code.py are defined here */ 
 
 #include <iostream>
-#include "cpp_hello_world.h"                                                    /* Creating a header file is optional, but recommended */
+#include "cpp_hello_world.h"	/* Creating a header file is optional, but recommended */
 
-void hello_world(){                                                             /* "hello world" is printed from the C function to the Python console */
+void hello_world(){		/* "hello world" is printed from the C function to the Python console */
     std::cout << std::endl << "hello world" << std::endl << std::endl;          
 } 
 
